47573183a5e198bfdc93e0a2488129deb7d382a7
EricNakamura <masaro.eric@gmail.com>
2024-03-04 20:34:58 +0000

    Adicionar arquivos de projeto.

---
+++ PlayboyRevelations.cpp
@@ -0,0 +1,13 @@
+﻿// PlayboyRevelations.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "PlayboyRevelations.h"
+#include "Player/Player.h"	
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

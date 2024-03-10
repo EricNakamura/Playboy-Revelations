926314965e207ab01d793d322023ae6865c1a770
EricNakamura <masaro.eric@gmail.com>
2024-03-10 22:10:23 +0000

    Player class works

--- include/Player.h
+++ include/Player.h
@@ -1,27 +1,40 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <string>
+#include <Atributes.h>
 
 namespace plr {
 	class Player {
 	private:
-		sf::Vector2f position;
-		sf::Vector2f size;
+		void DefaultInit();
+
+		sf::Vector2f position = sf::Vector2f(0,0);
+		sf::Vector2f size = sf::Vector2f(1.0f,1.0f);
 
 		int mass;
 
-		int health;
+		int health = 3;
 		sf::Texture texture;
 		sf::Sprite playerSprite;
 		int index;
 
+		plr::Atributes playerAtributes;
+
 
 	public:
 		Player();
-		Player(std::string texturePath);
+		Player(const std::string texturePath);
+		Player(sf::Texture texture);
 
-		void Update();
-		void LateUpdate();
+		void Update(float deltaTime);
+		void LateUpdate(float deltaTime);
+
+		void Draw(sf::RenderWindow& target);
+		sf::Sprite getSprite();
+		void setSpriteTexture(const sf::Texture);
+
+		void setPosition(sf::Vector2f vector);
+		void setSize(sf::Vector2f sizeVector);
 
 		void Move(sf::Vector2f vector);
 	};
--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -1,18 +1,66 @@
 #include <Player.h>
 #include <iostream>
+#include <Enums.h>
+#include <iostream>
 
 plr::Player::Player()
 {
+	DefaultInit();
 }
 
-plr::Player::Player(std::string texturePath)
+plr::Player::Player(const std::string texturePath)
 {
 	sf::Texture temp;
 	if (!temp.loadFromFile(texturePath)) {
 		std::cerr << "Impossible to load " << texturePath << std::endl;
+		return;
 	}
+
+	this->texture = temp;
+	this->playerSprite.setTexture(this->texture);
+	DefaultInit();
+}
+
+plr::Player::Player(sf::Texture texture) {
+	this->texture = texture;
+	this->setSpriteTexture(texture);
+	DefaultInit();
+}
+
+void plr::Player::setPosition(sf::Vector2f vector) {
+	this->playerSprite.setPosition(vector);
+}
+
+void plr::Player::setSize(sf::Vector2f sizeVector) {
+	this->playerSprite.setScale(sizeVector);
+}
+
+void plr::Player::DefaultInit() {
+	this->playerSprite.setScale(this->size);
+	this->playerSprite.setOrigin(this->texture.getSize().x/2.0f, this->texture.getSize().y/2.0f);
+}
+
+
+sf::Sprite plr::Player::getSprite() {
+	return this->playerSprite;
+}
+
+void plr::Player::setSpriteTexture(const sf::Texture texture) {
+	this->playerSprite.setTexture(texture);
+}
+
+void plr::Player::Draw(sf::RenderWindow& target) {
+	target.draw(this->getSprite());
 }
 
 void plr::Player::Move(sf::Vector2f vector)
+{
+}
+
+void plr::Player::Update(float deltaTime)
+{
+}
+
+void plr::Player::LateUpdate(float deltaTime)
 {
 }

3f29a5b29b993e7f0bf1d51d87b319252b6f4ed6
EricNakamura <masaro.eric@gmail.com>
2024-03-10 22:10:04 +0000

    Atributes class added

--- include/Atributes.h
+++ include/Atributes.h
@@ -1,11 +1,18 @@
 #pragma once
+#include <string>
+#include <Enums.h>
 
 namespace plr {
 	class Atributes {
 	private:
+		
 		int atributesLevels[5] = {1,1,1,1,1};
+		char* atributesTypes[5] = {"Camisa","Sapato","Cordão","Calça","Arma"};
 	public:
 		int* getAtributes();
 		int getAtributes(int selector);
+		char* getAtributesTypes(int selector);
+
+		void setAtributes(Enum::EAtributesTypes selector, int value);
 	};
 }
--- src/Player/Atributes.cpp
+++ src/Player/Atributes.cpp
@@ -1,4 +1,5 @@
-#include "Atributes.h"
+#include <Atributes.h>
+#include <iostream>
 
 int* plr::Atributes::getAtributes()
 {
@@ -7,5 +8,42 @@ int* plr::Atributes::getAtributes()
 
 int plr::Atributes::getAtributes(int selector)
 {
-    return this->atributesLevels[selector];
+    try {
+        if (selector > 4) {
+            throw ("Invalid Selector");
+        }
+        return this->atributesLevels[selector];
+    }
+    catch (std::string err) {
+        std::cerr << err << std::endl;
+    }
+}
+
+char* plr::Atributes::getAtributesTypes(int selector)
+{
+    try {
+        if (selector>4) {
+            throw ("Invalid Selector");
+        }
+        else
+            return this->atributesTypes[selector];
+    }
+    catch (std::string err) {
+        std::cerr << err << std::endl;
+    }
+}
+
+void plr::Atributes::setAtributes(Enum::EAtributesTypes selector, int value)
+{
+    try {
+        if (selector > 4) {
+            throw ("Invalid Selector");
+        }else if(value>3 || value < 1)
+            throw ("Invalid value given, should be between 1 and 3");
+        else
+            this->atributesLevels[selector] = value;
+    }
+    catch (std::string err) {
+        std::cerr << err << std::endl;
+    }
 }

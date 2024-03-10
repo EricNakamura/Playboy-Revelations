0d73049ec85118d9a836db583b6cd27ab7e9c829
EricNakamura <masaro.eric@gmail.com>
2024-03-10 22:10:47 +0000

    Enums for better index for atributes

---
+++ include/Enums.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace Enum {
+	/**	 
+	 <summary>Indexes all the 5 types of atributes that a player can have</summary>	 
+	*/
+	enum EAtributesTypes {
+		Shirt = 0,
+		Shoes = 1,
+		Necklaces = 2,
+		Pants = 3,
+		Weapon = 4
+	};
+}

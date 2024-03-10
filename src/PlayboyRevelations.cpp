5d50f3e548bf5bae73d4a192ce953b63417ddcc3
EricNakamura <masaro.eric@gmail.com>
2024-03-10 22:11:38 +0000

    Adjustments

--- src/PlayboyRevelations.cpp
+++ src/PlayboyRevelations.cpp
@@ -2,17 +2,28 @@
 //
 
 #include <SFML/Graphics.hpp>
-#include <Player.h>	
+#include <Player.h>
+#include <Camera.h>
+#include <iostream>
+#include <Game.h>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Playboy Revelations", sf::Style::Default);
 	sf::Clock clock;
+
+	sf::RenderWindow window(sf::VideoMode(800,600), "Playboy Revelation", sf::Style::Default);
+
+	window.setVerticalSyncEnabled(true);
+
+	Initilize();
+
+	plr::Camera camera(1.f);
 
 	while (window.isOpen())
 	{
 		float deltaTime = clock.restart().asSeconds();
 		sf::Event event{};
+
 
 		while (window.pollEvent(event))
 		{
@@ -20,9 +31,11 @@ int main()
 				window.close();
 			}
 		}
+		Update(deltaTime);
 
+		window.setView(camera.getView(window.getSize()));
 		window.clear();
-
+		Render(window);
 		window.display();
 	}
 

49014b363beb8c82c2f22ba8b74a2e9dc386ccb4
EricNakamura <masaro.eric@gmail.com>
2024-03-11 03:26:39 +0000

    Position feature added to the camera

--- include/Camera.h
+++ include/Camera.h
@@ -4,10 +4,13 @@
 namespace plr {
 	class Camera {
 	private:
-		float zoomLevel = 5.0f*1000;
+		float zoomLevel = 5.0f;
+		sf::Vector2<float> position = sf::Vector2f(0,0);
 	public:
 		sf::View getView(sf::Vector2u windowSize);
 		Camera(float zoomLevel);
 		Camera();
+
+		void setPosition(sf::Vector2<float> position);
 	};
 }
--- src/Camera/Camera.cpp
+++ src/Camera/Camera.cpp
@@ -1,15 +1,20 @@
 #include <Camera.h>
 
-plr::Camera::Camera(float zoomLevel) : zoomLevel(zoomLevel*1000) {}
+plr::Camera::Camera(float zoomLevel) : zoomLevel(zoomLevel) {}
 plr::Camera::Camera() {}
 
 sf::View plr::Camera::getView(sf::Vector2u windowSize) {
 	float aspect = (float)windowSize.x / (float)windowSize.y;
 	sf::Vector2f size;
 	if (aspect < 1.0f)
-		size = sf::Vector2f(this->zoomLevel, this->zoomLevel / aspect);
+		size = sf::Vector2f(zoomLevel, zoomLevel / aspect);
 	else
-		size = sf::Vector2f(this->zoomLevel * aspect, this->zoomLevel);
+		size = sf::Vector2f(zoomLevel * aspect, zoomLevel);
 
-	return sf::View(sf::Vector2f(windowSize.x/2, windowSize.y/2), size);
+	return sf::View(position, size);
+}
+
+void plr::Camera::setPosition(sf::Vector2<float> position)
+{
+	this->position = position;
 }

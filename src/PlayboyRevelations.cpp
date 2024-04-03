77961e74677ff82dae119a08e9301c93e0ed8c3d
EricNakamura <masaro.eric@gmail.com>
2024-04-03 15:33:52 +0100

    Initialize cycle chenged

--- src/PlayboyRevelations.cpp
+++ src/PlayboyRevelations.cpp
@@ -2,22 +2,25 @@
 //
 
 #include <SFML/Graphics.hpp>
-#include <Player.h>
+#include <GameMap.h>
+#include <Entity.h>
 #include <Camera.h>
 #include <iostream>
 #include <Game.h>
 
 int main()
 {
+
+	bool started = false;
 	sf::Clock clock;
 
-	sf::RenderWindow window(sf::VideoMode(800,600), "Playboy Revelation", sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Playboy Revelation", sf::Style::Default);
 
 	window.setVerticalSyncEnabled(true);
 
-	Initilize();
 
-	plr::Camera camera(50.f);
+	static plr::Camera camera(300.f);
+
 
 	while (window.isOpen())
 	{
@@ -27,18 +30,22 @@ int main()
 
 		while (window.pollEvent(event))
 		{
+
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
 		}
-		Update(deltaTime);
-
 		window.setView(camera.getView(window.getSize()));
 		window.clear();
+		if (!started)
+		{
+			Initilize(window);
+			started = true;
+		}
 		Update(deltaTime);
 		Render(window);
-		LateUpdate(deltaTime);
 		window.display();
+		LateUpdate(deltaTime);
 	}
 
 	return 0;
